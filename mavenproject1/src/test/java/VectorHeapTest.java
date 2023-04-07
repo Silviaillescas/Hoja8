41b6be9d67890784f123fc323fa610f0fcd8ff79
Silviaillescas <silviaillesca402@gmail.com>
2023-04-06 21:12:28 -0600

    JUnit

--- mavenproject1/src/test/java/VectorHeapTest.java
+++ mavenproject1/src/test/java/VectorHeapTest.java
@@ -7,6 +7,33 @@
  *
  * @author silvi
  */
-public class VectorHeapTest {
+import Hoja8.VectorHeap;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class VectorHeapTest<E> {
     
+    @Test
+    public void isEmptyTest(){
+        VectorHeap myVectorHeap = new VectorHeap();
+        boolean expectedResult = true;
+        boolean result = myVectorHeap.isEmpty();
+        assertEquals(expectedResult, result);
+    }
+    
+@Test
+    public void sizeTest() {
+        VectorHeap myVectorHeap = new VectorHeap();
+        int expectedResult = 0;
+        int result = myVectorHeap.size();
+        assertEquals(expectedResult, result);
+    }
+
+
+
 }
+
+    
+
+
