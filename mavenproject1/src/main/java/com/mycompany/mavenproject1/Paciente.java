1fdf2c6597c6344e3d59765d7b1aae89ecb22e25
Silviaillescas <silviaillesca402@gmail.com>
2023-04-06 18:46:28 -0600

    Clase paciente

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Paciente.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Paciente.java
@@ -8,6 +8,25 @@ package com.mycompany.mavenproject1;
  *
  * @author silvi
  */
-public class Paciente {
-    
+public class Paciente implements Comparable<Paciente> {
+    String sintoma;
+    String codigo;
+    String nombre; 
+
+    Paciente(String nombre, String sintoma, String codigo) {
+        this.sintoma = sintoma;
+         this.codigo = codigo;
+        this.nombre = nombre;
+    }
+
+    @Override
+    public int compareTo(Paciente paciente) {
+        return codigo.compareToIgnoreCase(paciente.codigo);
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " " + sintoma + " " + codigo;
+    }
 }
+
