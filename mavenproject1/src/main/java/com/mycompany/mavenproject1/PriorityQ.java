a9dd435ca27b5cea48335fddb8215b2f87bf9ad9
Silviaillescas <silviaillesca402@gmail.com>
2023-04-06 18:57:40 -0600

    Priority

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/PriorityQ.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/PriorityQ.java
@@ -1,13 +1,26 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package com.mycompany.mavenproject1;
 
 /**
  *
  * @author silvi
+ * @param <E>
  */
-public class PriorityQ {
-    
+
+public interface PriorityQ<E extends Comparable<E>>
+{
+	public E getFirst();
+	
+	public E remove();
+        public boolean isEmpty();
+	
+	public int size();
+	
+	public void clear();
+
+	public void add(E value);
+
 }
